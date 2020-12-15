5b98bc155b7a16d6414eea1824937d13e6ef89db
Charles An <mucollabo@gmail.com>
2020-12-15 20:14:06 +0900

    study until 12.6.2

---
+++ style3.py
@@ -0,0 +1,15 @@
+"모듈 docstring"
+
+
+def func():
+    "함수 docstring. 안녕, 엄마!"
+    first = 1
+    second = 2
+    third = 3
+    print(first)
+    print(second)
+    print(third)
+
+
+if __name__ == '__main__':
+    func()
